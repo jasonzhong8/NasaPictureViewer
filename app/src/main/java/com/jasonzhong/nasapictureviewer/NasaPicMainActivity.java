9241a79271e73c7af86b20a56ce8e51991ea3925
junzhong <jasonzhong2000@gmail.com>
2017-07-28 13:43:24 -0400

    Initial commit

---
+++ app/src/main/java/com/jasonzhong/nasapictureviewer/NasaPicMainActivity.java
@@ -0,0 +1,23 @@
+package com.jasonzhong.nasapictureviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NasaPicMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_nasa_pic_main);
+
+    }
+
+
+}
